95ba74889b83917dcf4e08dfa07db1e2a6b4fa8b
Jedrzej Koriat <jedrzej.koriat@gmail.com>
2024-07-17 20:02:46 +0200

    Changed: exercises module

--- Data/Exercise.cs
+++ Data/Exercise.cs
@@ -2,15 +2,9 @@
 {
 	public class Exercise
 	{
-        public string? Name { get; set; }
 		public int Id { get; set; }
-		public string? MainExerciseName { get; set; }
-        public int? MainExerciseNumberOfRepeats { get; set; }
-        public string? MainExerciseVideoLink { get; set; }
-		public string? AdditionalExerciseName { get; set; }
-        public int? AdditionalExerciseNumberOfRepeats { get; set; }
-        public string? AdditionalExerciseVideoLink { get; set; }
-        public int? OverallNumberOfSets { get; set; }
+		public string? Name { get; set; }
+		public string? VideoLink { get; set; }
         public string? Description { get; set; }
 	}
 }
--- Models/ExerciseVM.cs
+++ Models/ExerciseVM.cs
@@ -8,27 +8,9 @@ namespace TrainingPlanApp.Web.Models
         [Display(Name = "Name")]
         [Required]
         public string Name { get; set; }
-        [Display(Name = "Exercise")]
-        [Required]
-        public string? MainExerciseName { get; set; }
-        [Display(Name = "Repeats")]
-        [Required]
-        public int MainExerciseNumberOfRepeats { get; set; }
         [Display(Name = "Video")]
         [Required]
-        public string? MainExerciseVideoLink { get; set; }
-        [Display(Name = "Additional exercise")]
-        [Required]
-        public string? AdditionalExerciseName { get; set; }
-        [Display(Name = "Repeats")]
-        [Required]
-        public int? AdditionalExerciseNumberOfRepeats { get; set; }
-        [Display(Name = "Video")]
-        [Required]
-        public string? AdditionalExerciseVideoLink { get; set; }
-        [Display(Name = "Number of sets")]
-        [Required]
-        public int OverallNumberOfSets { get; set; }
+        public string? VideoLink { get; set; }
         [Display(Name = "Description")]
         public string? Description { get; set; }
     }
