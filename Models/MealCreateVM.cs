734753b1b7912ac50e0712a8efb0e96c39ac4c0b
jedrulcia <jedrzej.koriat@gmail.com>
2024-09-08 19:14:33 +0200

    Changed: MealCreateVM and MealCreate View

--- Models/MealCreateVM.cs
+++ Models/MealCreateVM.cs
@@ -9,22 +9,8 @@ namespace TrainingPlanApp.Web.Models
         public int? Id { get; set; }
         [Display(Name = "Name")]
         public string? Name { get; set; }
-        [Display(Name = "Amount of kcal")]
-        public int? Kcal { get; set; }
-        [Display(Name = "Amount of proteins")]
-        public int? Protein { get; set; }
-        [Display(Name = "Amount of fats")]
-        public int? Fat { get; set; }
-        [Display(Name = "Amount of carbs")]
-        public int? Carbs { get; set; }
-        [Display(Name = "List of ingredients")]
-        public List<IngredientVM>? Ingredients { get; set; }
-        [Display(Name = "Ingredient")]
-        public string? IngredientName { get; set; }
-        [Display(Name = "Serving size (g)")]
-        public int? IngredientServingSize {  get; set; }
-        [Display(Name = "Description")]
-        public string? Description { get; set; }
+        [Display(Name = "Recipe")]
+        public string? Recipe { get; set; }
 
     }
 }
