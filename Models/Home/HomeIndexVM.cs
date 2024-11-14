417677e0118267014c2bb9dea286e830a7b6c9fd
jedrulcia <jedrzej.koriat@gmail.com>
2024-11-14 21:56:54 +0100

    added chart to home

---
+++ Models/Home/HomeIndexVM.cs
@@ -0,0 +1,12 @@
+﻿using EliteAthleteApp.Models.Charts;
+using EliteAthleteApp.Models.User;
+
+namespace EliteAthleteApp.Models.Home
+{
+	public class HomeIndexVM
+	{
+		public UserChartsVM? UserChartsVM { get; set; }
+		public bool IsLoggedIn { get; set; } = false;
+		public UserVM? UserVM { get; set; }
+	}
+}
