3a37b2c0ec4532080ff55a9b4e5b02ce3dea3525
jedrulcia <jedrzej.koriat@gmail.com>
2024-11-17 21:15:03 +0100

    frontend changes

---
+++ Models/Home/HomePanelVM.cs
@@ -0,0 +1,11 @@
+﻿using EliteAthleteApp.Models.Charts;
+using EliteAthleteApp.Models.User;
+
+namespace EliteAthleteApp.Models.Home
+{
+    public class HomePanelVM
+    {
+        public UserVM UserVM { get; set; }
+        public UserChartsVM UserChartsVM { get; set; }
+    }
+}
